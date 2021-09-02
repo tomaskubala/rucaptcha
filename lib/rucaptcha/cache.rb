2aaec358267ad6eebed3297f7ba1cce0c967b4ef
Tomáš Kubala <mail@tomaskubala.sk>
2021-09-02 14:27:09 +0200

    Prefer Rails.cache over ActiveSupport::Cache.lookup_store

--- lib/rucaptcha/cache.rb
+++ lib/rucaptcha/cache.rb
@@ -5,7 +5,10 @@ module RuCaptcha
     def cache
       return @cache if defined? @cache
 
-      @cache = ActiveSupport::Cache.lookup_store(RuCaptcha.config.cache_store)
+      rails_cache = Rails.cache
+      lookuped_store = ActiveSupport::Cache.lookup_store(RuCaptcha.config.cache_store)
+
+      @cache = rails_cache.class == lookuped_store.class ? rails_cache : lookuped_store
       @cache
     end
   end
